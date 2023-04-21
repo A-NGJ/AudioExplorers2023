d42844756522d9af2b2cc3957997e498ac95e335
Piotrzems <31177032+Piotrzems@users.noreply.github.com>
2023-04-21 15:18:50 +0200

    1D_CNN_MultiHeaded; MobileNet attempt

--- models.py
+++ models.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 keras = tf.keras
 from keras import layers, Input
 from keras.models import Model
+from tensorflow.keras.applications import MobileNet
+
 import TransformerEncoder
 
 
@@ -48,6 +50,10 @@ class ModelFactory:
             return create_cnn_model_optimized(self.input_shape, self.num_classes)
         elif model_type ==  "Transformer":
             return create_transformer_model(self.input_shape, self.num_classes)
+        elif model_type == "CNN_1D_MultiHeaded":
+            return create_1Dcnn_MultiHeaded_model(self.input_shape, self.num_classes)
+        # elif model_type == "MobileNet":
+        #     return create_MobileNet_model(self.input_shape, self.num_classes)
         else:
             raise ValueError(f"Unknown model type: {model_type}")
 
@@ -262,4 +268,110 @@ def create_transformer_model(input_shape: tuple, num_classes: int, name: str = "
 
     # Define and compile model
     model = Model(inputs=inputs, outputs=outputs, name=name)
+    return model
+
+def create_1Dcnn_MultiHeaded_model(input_shape: tuple, num_classes: int, name: str = "CNN_1D_MultiHeaded") -> Model:
+    """
+    Creates a 1D CNN Multi_headed model
+    
+    Parameters
+    ----------
+    input_shape : tuple
+        Shape of input data
+    num_classes : int
+        Number of classes to predict
+    """
+    input_layer = tf.keras.layers.Input(shape=(32, 96))
+
+    head1 = tf.keras.layers.Conv1D(32, 3, activation='relu')(input_layer)
+    head1 = tf.keras.layers.BatchNormalization()(head1)
+    head1 = tf.keras.layers.MaxPooling1D(2)(head1)
+    head1 = tf.keras.layers.Conv1D(64, 3, activation='relu')(head1)
+    head1 = tf.keras.layers.BatchNormalization()(head1)
+    head1 = tf.keras.layers.MaxPooling1D(2)(head1)
+    head1 = tf.keras.layers.Conv1D(128, 3, activation='relu')(head1)
+    head1 = tf.keras.layers.BatchNormalization()(head1)
+    head1 = tf.keras.layers.MaxPooling1D(2)(head1)
+    
+    head1 = tf.keras.layers.Conv1D(256, 3, activation='relu', padding='same')(head1)  # Add padding='same'
+    head1 = tf.keras.layers.BatchNormalization()(head1)
+    head1 = tf.keras.layers.MaxPooling1D(2)(head1)
+    
+    head1 = tf.keras.layers.Conv1D(256, 3, activation='relu', padding='same')(head1)  # Add padding='same'
+    head1 = tf.keras.layers.BatchNormalization()(head1)
+    # head1 = tf.keras.layers.MaxPooling1D(2)(head1)
+
+
+    head1 = tf.keras.layers.Flatten()(head1)
+
+    head2 = tf.keras.layers.Conv1D(32, 3, activation='relu')(input_layer)
+    head2 = tf.keras.layers.BatchNormalization()(head2)
+    head2 = tf.keras.layers.MaxPooling1D(2)(head2)
+    head2 = tf.keras.layers.Conv1D(64, 3, activation='relu')(head2)
+    head2 = tf.keras.layers.BatchNormalization()(head2)
+    head2 = tf.keras.layers.MaxPooling1D(2)(head2)
+    head2 = tf.keras.layers.Conv1D(128, 3, activation='relu')(head2)
+    head2 = tf.keras.layers.BatchNormalization()(head2)
+    head2 = tf.keras.layers.MaxPooling1D(2)(head2)
+    head2 = tf.keras.layers.Flatten()(head2)
+
+    head3 = tf.keras.layers.Conv1D(32, 3, activation='relu')(input_layer)
+    head3 = tf.keras.layers.BatchNormalization()(head3)
+    head3 = tf.keras.layers.MaxPooling1D(2)(head3)
+    head3 = tf.keras.layers.Conv1D(64, 3, activation='relu')(head3)
+    head3 = tf.keras.layers.BatchNormalization()(head3)
+    head3 = tf.keras.layers.MaxPooling1D(2)(head3)
+    head3 = tf.keras.layers.Conv1D(128, 3, activation='relu')(head3)
+    head3 = tf.keras.layers.BatchNormalization()(head3)
+    head3 = tf.keras.layers.MaxPooling1D(2)(head3)
+    head3 = tf.keras.layers.Flatten()(head3)
+
+    # Concatenate heads
+    merged = tf.keras.layers.Concatenate()([head1])
+    merged = tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(l=0.01))(merged)
+    merged = tf.keras.layers.Dropout(0.5)(merged)
+    output_layer = tf.keras.layers.Dense(5, activation='softmax')(merged)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+
+    # lr_schedule = ExponentialDecay(initial_learning_rate=1e-3, decay_steps=10000, decay_rate=0.9)
+    # optimizer = Adam(learning_rate=lr_schedule)
+    # model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+    
+def create_MobileNet_model(input_shape: tuple, num_classes: int, name: str = "MobileNet") -> Model:
+    """
+    Creates a MobileNet model - Work in progress
+    
+    Parameters
+    ----------
+    input_shape : tuple
+        Shape of input data
+    num_classes : int
+        Number of classes to predict
+    """
+    # Load MobileNet without the top classification layer and set input shape
+    print(input_shape)
+    input_tensor = Input(shape=input_shape)
+    base_model = MobileNet(include_top=False, input_shape=input_tensor, weights='imagenet')
+
+    # Add a custom classification layer
+    x = base_model.output
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(128, activation='relu')(x)
+    predictions = layers.Dense(num_classes, activation='softmax')(x)
+
+        # Freeze the base model layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # Fine-tuning: Unfreeze the top 30 layers of the base model
+    for layer in base_model.layers[:-30]:
+        layer.trainable = False
+    for layer in base_model.layers[-30:]:
+        layer.trainable = True
+
+    model = tf.keras.Model(inputs=base_model.input, outputs=predictions)
+
+
     return model
